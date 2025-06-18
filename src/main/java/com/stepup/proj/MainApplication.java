b23ff6bba82b8794cd8870c1e8e6500efac1dabc
Sergey Bogomolov <freebios@yandex.ru>
2025-06-19 00:02:43 +0300

    Доработка логики проекта

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -3,6 +3,6 @@ package com.stepup.proj;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Привет, мир!");
-        System.out.println("123456789");
+        System.out.println("12345678");
     }
 }
